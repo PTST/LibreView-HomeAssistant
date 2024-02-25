5b5a57660d929c5550c8e538c53518eba619a7f0
Patrick Toft Steffensen <17211264+PTST@users.noreply.github.com>
2024-02-25 18:07:48 +0000

    Binary sensor

--- custom_components/libreview/binary_sensor.py
+++ custom_components/libreview/binary_sensor.py
@@ -1,23 +1,17 @@
-from datetime import datetime, timedelta, timezone
 from typing import Dict
 from uuid import UUID
 
 from homeassistant.components.binary_sensor import BinarySensorEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
-from LibreView.models import Connection, GlucoseMeasurement, Sensor
+from LibreView.models import Connection, GlucoseMeasurement
 
 from .const import (
-    CONF_SENSOR_DURATION,
-    CONF_SHOW_TREND_ARROW,
     CONF_UOM,
     DEFAULT_ICON,
     DOMAIN,
-    SENSOR_ICON,
-    TREND_ICONS,
     GlucoseUnitOfMeasurement,
 )
 from .coordinator import LibreViewCoordinator
@@ -28,7 +22,6 @@ async def async_setup_entry(
     entry: ConfigEntry,
     async_add_entities: AddEntitiesCallback,
 ) -> None:
-    """Set up G4S binary sensors based on a config entry."""
     coordinator: LibreViewCoordinator = hass.data[DOMAIN][entry.entry_id]
     uom = GlucoseUnitOfMeasurement(entry.data[CONF_UOM])
 
@@ -39,7 +32,6 @@ async def async_setup_entry(
         GlucoseLowSensor(coordinator, connection_id, uom)
         for connection_id, _ in coordinator.data["glucose_readings"].items()
     ]
-
     async_add_entities(sensors)
 
 
@@ -54,9 +46,43 @@ class GlucoseHighSensor(CoordinatorEntity, BinarySensorEntity):
         uom: GlucoseUnitOfMeasurement,
     ):
         super().__init__(coordinator)
-        self._attr_unique_id = f"{connection_id}_glucose_reading"
+        self._attr_unique_id = f"{connection_id}_glucose_reading_is_high"
         self.connection_id = connection_id
         self.uom = uom
+
+    @property
+    def icon(self):
+        return DEFAULT_ICON
+
+    @property
+    def connection(self) -> Connection:
+        return self.coordinator.data["glucose_readings"][self.connection_id]
+
+    @property
+    def gcm(self) -> GlucoseMeasurement:
+        return self.connection.glucose_measurement
+
+    @property
+    def trend_arrow(self) -> int:
+        return self.gcm.trend_arrow
+
+    @property
+    def name(self) -> str:
+        """Return the name of the entity."""
+        name = f"{self.connection.first_name } {self.connection.last_name}"
+        return f"{name} glucose level is high"
+
+    @property
+    def is_on(self) -> bool:
+        """Return the state of the entity."""
+        return self.gcm.value_in_mg_per_dl > self.connection.target_high
+
+    @property
+    def extra_state_attributes(self) -> Dict[str, int | float]:
+        return {
+            "target_high_mmol_l": round(self.connection.target_high / 18, 1),
+            "target_high_mg_dl": self.connection.target_high,
+        }
 
 
 class GlucoseLowSensor(CoordinatorEntity, BinarySensorEntity):
@@ -70,6 +96,40 @@ class GlucoseLowSensor(CoordinatorEntity, BinarySensorEntity):
         uom: GlucoseUnitOfMeasurement,
     ):
         super().__init__(coordinator)
-        self._attr_unique_id = f"{connection_id}_glucose_reading"
+        self._attr_unique_id = f"{connection_id}_glucose_reading_is_low"
         self.connection_id = connection_id
         self.uom = uom
+
+    @property
+    def icon(self):
+        return DEFAULT_ICON
+
+    @property
+    def connection(self) -> Connection:
+        return self.coordinator.data["glucose_readings"][self.connection_id]
+
+    @property
+    def gcm(self) -> GlucoseMeasurement:
+        return self.connection.glucose_measurement
+
+    @property
+    def trend_arrow(self) -> int:
+        return self.gcm.trend_arrow
+
+    @property
+    def name(self) -> str:
+        """Return the name of the entity."""
+        name = f"{self.connection.first_name } {self.connection.last_name}"
+        return f"{name} glucose level is low"
+
+    @property
+    def is_on(self) -> bool:
+        """Return the state of the entity."""
+        return self.gcm.value_in_mg_per_dl > self.connection.target_low
+
+    @property
+    def extra_state_attributes(self) -> Dict[str, int | float]:
+        return {
+            "target_low_mmol_l": round(self.connection.target_low / 18, 1),
+            "target_low_mg_dl": self.connection.target_low,
+        }
